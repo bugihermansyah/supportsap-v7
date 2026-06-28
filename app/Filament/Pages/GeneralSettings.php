fa59c332bb83287940e1a3646b91585810e59c4b
bugihermansyah <bugihermansyah@gmail.com>
2026-06-28 20:29:38 +0700

    update kpi parameter

--- app/Filament/Pages/GeneralSettings.php
+++ app/Filament/Pages/GeneralSettings.php
@@ -3,9 +3,13 @@
 namespace App\Filament\Pages;
 
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\ViewField;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Schema;
 use Inerba\DbConfig\AbstractPageSettings;
 
@@ -15,6 +19,8 @@ class GeneralSettings extends AbstractPageSettings
      * @var array<string, mixed> | null
      */
     public ?array $data = [];
+
+
 
     protected static ?string $title = 'General Settings';
 
@@ -46,35 +52,143 @@ class GeneralSettings extends AbstractPageSettings
             'brand_name' => '',
             'brand_logo' => '',
             'site_favicon' => '',
+            // KPI Parameters
+            'kpi_base_score' => 100,
+            'kpi_late_penalty_h1' => 10,
+            'kpi_late_penalty_h2' => 20,
+            'kpi_late_penalty_h3' => 50,
+            'kpi_no_photo_penalty' => 15,
+            'kpi_no_form_penalty' => 30,
+            'kpi_sameday_bonus' => 15,
+            'kpi_bonus_very_hard' => 15,
+            'kpi_bonus_hard' => 10,
+            'kpi_grade_a_plus_min' => 101,
+            'kpi_grade_a_min' => 85,
+            'kpi_grade_b_min' => 70,
+            'kpi_grade_c_min' => 50,
         ];
     }
+
+
 
     public function form(Schema $schema): Schema
     {
         return $schema
             ->components([
-                Section::make('Brand')
-                    ->description('Konfigurasi brand aplikasi')
-                    ->icon('heroicon-o-building-office')
-                    ->columns(2)
-                    ->schema([
-                        TextInput::make('brand_name')
-                            ->label('Brand Name')
-                            ->placeholder('Masukkan nama brand')
-                            ->columnSpanFull(),
+                Tabs::make('Settings')
+                    ->tabs([
+                        Tab::make('Brand')
+                            ->icon('heroicon-o-building-office')
+                            ->schema([
+                                Section::make('Brand')
+                                    ->description('Konfigurasi brand aplikasi')
+                                    ->columns(2)
+                                    ->schema([
+                                        TextInput::make('brand_name')
+                                            ->label('Brand Name')
+                                            ->placeholder('Masukkan nama brand')
+                                            ->columnSpanFull(),
 
-                        FileUpload::make('brand_logo')
-                            ->label('Brand Logo')
-                            ->image()
-                            ->directory('settings')
-                            ->maxSize(2048),
+                                        FileUpload::make('brand_logo')
+                                            ->label('Brand Logo')
+                                            ->image()
+                                            ->directory('settings')
+                                            ->maxSize(2048),
 
-                        FileUpload::make('site_favicon')
-                            ->label('Favicon')
-                            ->image()
-                            ->directory('settings')
-                            ->maxSize(1024),
-                    ]),
+                                        FileUpload::make('site_favicon')
+                                            ->label('Favicon')
+                                            ->image()
+                                            ->directory('settings')
+                                            ->maxSize(1024),
+                                    ]),
+                            ]),
+                        Tab::make('KPI Parameters')
+                            ->icon('heroicon-o-chart-bar')
+                            ->schema([
+                                Section::make('Nilai Dasar & Batas Grade')
+                                    ->columns(4)
+                                    ->schema([
+                                        TextInput::make('kpi_base_score')
+                                            ->label('Skor Dasar')
+                                            ->numeric()
+                                            ->minValue(1)
+                                            ->required(),
+
+                                        TextInput::make('kpi_grade_a_min')
+                                            ->label('Min. Grade A')
+                                            ->numeric()
+                                            ->required(),
+
+                                        TextInput::make('kpi_grade_b_min')
+                                            ->label('Min. Grade B')
+                                            ->numeric()
+                                            ->required(),
+
+                                        TextInput::make('kpi_grade_c_min')
+                                            ->label('Min. Grade C')
+                                            ->numeric()
+                                            ->required(),
+                                    ]),
+
+                                Section::make('Bonus Level & Inisiatif')
+                                    ->columns(3)
+                                    ->schema([
+                                        TextInput::make('kpi_bonus_hard')
+                                            ->label('Level Hard')
+                                            ->prefix('+')
+                                            ->numeric()
+                                            ->required(),
+
+                                        TextInput::make('kpi_bonus_very_hard')
+                                            ->label('Level Very Hard')
+                                            ->prefix('+')
+                                            ->numeric()
+                                            ->required(),
+
+                                        TextInput::make('kpi_sameday_bonus')
+                                            ->label('Sameday Progress')
+                                            ->prefix('+')
+                                            ->numeric()
+                                            ->required(),
+                                    ]),
+
+                                Section::make('Penalti Report')
+                                    ->columns(5)
+                                    ->schema([
+                                        TextInput::make('kpi_late_penalty_h1')
+                                            ->label('Report H+1')
+                                            ->prefix('-')
+                                            ->numeric()
+                                            ->required(),
+
+                                        TextInput::make('kpi_late_penalty_h2')
+                                            ->label('Report H+2')
+                                            ->prefix('-')
+                                            ->numeric()
+                                            ->required(),
+
+                                        TextInput::make('kpi_late_penalty_h3')
+                                            ->label('Report H+3 ke atas')
+                                            ->prefix('-')
+                                            ->numeric()
+                                            ->required(),
+
+                                        TextInput::make('kpi_no_photo_penalty')
+                                            ->label('Tidak Ada Foto')
+                                            ->prefix('-')
+                                            ->numeric()
+                                            ->required(),
+
+                                        TextInput::make('kpi_no_form_penalty')
+                                            ->label('Tidak Ada Form Support')
+                                            ->prefix('-')
+                                            ->numeric()
+                                            ->required(),
+                                    ]),
+                            ]),
+
+                    ])
+                    ->persistTabInQueryString(),
             ])
             ->statePath('data');
     }
